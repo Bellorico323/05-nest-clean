4f7862e143c2ac9fa0a6ae6351f4ea2ec141d992
Bellorico323 <mabo.orico@gmail.com>
2024-05-29 23:24:03 -0300

    last commit

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -4,18 +4,27 @@ import { PrismaClient } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
 import { execSync } from 'node:child_process'
 import { DomainEvents } from '@/core/events/domain-events'
+import { Redis } from 'ioredis'
+import { envSchema } from '@/infra/env/env'
 
 config({ path: '.env', override: true })
 config({ path: '.env.test', override: true })
 
-const prisma = new PrismaClient()
+const env = envSchema.parse(process.env)
 
-function generateUniqueDatabaseUrl(schemaId: string) {
-  if (!process.env.DATABASE_URL) {
-    throw new Error('Please provide DATABASE_URL environment variable.')
+const prisma = new PrismaClient()
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+})
+
+function generateUniqueDatabaseURL(schemaId: string) {
+  if (!env.DATABASE_URL) {
+    throw new Error('Please provider a DATABASE_URL environment variable')
   }
 
-  const url = new URL(process.env.DATABASE_URL)
+  const url = new URL(env.DATABASE_URL)
 
   url.searchParams.set('schema', schemaId)
 
@@ -25,13 +34,15 @@ function generateUniqueDatabaseUrl(schemaId: string) {
 const schemaId = randomUUID()
 
 beforeAll(async () => {
-  const databaseURl = generateUniqueDatabaseUrl(schemaId)
+  const databaseURL = generateUniqueDatabaseURL(schemaId)
 
-  process.env.DATABASE_URL = databaseURl
+  process.env.DATABASE_URL = databaseURL
 
   DomainEvents.shouldRun = false
 
-  execSync('npx prisma migrate deploy')
+  await redis.flushdb()
+
+  execSync('pnpm prisma migrate deploy')
 })
 
 afterAll(async () => {
